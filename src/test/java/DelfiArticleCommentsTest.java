a816d9e7b51e64a06e825355e1793e8112975098
Dmitrijs Braslis <test@test.lv>
2021-05-12 21:15:13 +0300

    delfi test

--- src/test/java/DelfiArticleCommentsTest.java
+++ src/test/java/DelfiArticleCommentsTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,39 +13,75 @@ import java.util.List;
 public class DelfiArticleCommentsTest {
     private final By ACCEPT_COOKIE_BTN = By.xpath(".//button[@mode = 'primary']");
     private final By HOME_PAGE_TITLE = By.xpath(".//h1[contains(@class, 'headline__title')]");
+    private final By HOME_PAGE_COMMENTS = By.xpath(".//a[contains(@class, 'comment-count')]");
+    private final By HOME_PAGE_ARTICLE = By.tagName("article");
+
+    private final By ARTICLE_PAGE_TITLE = By.xpath(".//h1[contains(@class, 'text-size-md-30')]");
+    private final By ARTICLE_PAGE_COMMENTS = By.xpath(".//a[contains(@class, 'text-size-md-28')]");
+
+    private WebDriver driver;
 
     @Test
     public void titleAndCommentsCountCheck() {
         System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
 
         driver.get("http://delfi.lv");
 
+        //---------------------------HOME PAGE-----------------------------------------------------------
         WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIE_BTN));
 
         driver.findElement(ACCEPT_COOKIE_BTN).click();
 
-        List<WebElement> titles = driver.findElements(HOME_PAGE_TITLE);
+        List<WebElement> articles = driver.findElements(HOME_PAGE_ARTICLE);
+        WebElement article = articles.get(4);
 
-        //----------------------FOR----------------------------------------------------------
-        for (int i = 0; i < titles.size(); i++) {  //i = i + 1   ->  i++
-            if (!titles.get(i).getText().isEmpty()) {  // !true = false   !false = true
-                System.out.println(i + ": " + titles.get(i).getText());
-            }
+        String homePageTitle = article.findElement(HOME_PAGE_TITLE).getText();
+        int homePageCommentsCount = getCommentsCount(article, HOME_PAGE_COMMENTS);
+
+        article.findElement(HOME_PAGE_TITLE).click();
+
+        //-------------------------ARTICLE PAGE----------------------------------------------------------
+        String articlePageTitle = driver.findElement(ARTICLE_PAGE_TITLE).getText();
+        int articlePageCommentsCount = getCommentsCount(ARTICLE_PAGE_COMMENTS);
+
+        Assertions.assertEquals(homePageTitle, articlePageTitle, "Wrong title!");
+        Assertions.assertEquals(homePageCommentsCount, articlePageCommentsCount, "Wrong comments count!");
+
+        //-------------------------COMMENTS PAGE---------------------------------------------------------
+        //...
+    }
+
+    private int getCommentsCount(By locator) {
+        int commentsCount = 0;
+
+        if (!driver.findElements(locator).isEmpty()) {
+            commentsCount = removeBrackets(driver.findElement(locator));
         }
 
-        //-------------------FOREACH---------------------------------------------------------
-        for (WebElement we : titles) {
-//            if (!we.getText().isEmpty()) {
-//                System.out.println(we.getText());
-//            } else {
-//                System.out.println("----------------------------------------");
-//            }
+        return commentsCount;
+    }
 
-            // условие ? true : false (else)
-            System.out.println(we.getText().isEmpty() ? "---------" : we.getText());
+    private int getCommentsCount(WebElement we, By locator) {
+        int commentsCount = 0;
+
+        if (!we.findElements(locator).isEmpty()) {
+            commentsCount = removeBrackets(we.findElement(locator));
         }
+
+        return commentsCount;
+    }
+
+    private int removeBrackets(WebElement we) {
+        String commentsCountText = we.getText();
+        commentsCountText = commentsCountText.substring(1, commentsCountText.length() - 1); // (36) -> 36 (String)
+        return Integer.parseInt(commentsCountText); // 36 (String) -> 36 (int)
+    }
+
+    @AfterEach
+    public void closeBrowser() {
+        driver.close();
     }
 }
